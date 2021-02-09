557aba515e4fd3156ac939ac1e6f63b035fa610a
Lisa Worsham <lisa.r.hyndman@gmail.com>
2021-02-09 10:17:51 -0600

    added partie to projects

--- src/info/projects.js
+++ src/info/projects.js
@@ -1,8 +1,16 @@
 import critterSitterImg from "../pictures/crittersitter.PNG"
 import litWineImg from "../pictures/litandwine.png"
-import noteTakerImg from "../pictures/notetaker.PNG"
+import partieImg from "../pictures/partie.png"
 
 const projects = [
+    {
+        "id": 3,
+        "projectName": "Partie",
+        "exampleImg": partieImg,
+        "summary": "A party planning REACTJS app made for event planners where they can keep track of their upcoming planned events, track the budget, todos, invoices and RSVPs if needed, all on a clean, clutter free dashboard.",
+        "githubLink": "https://github.com/lisaworsham/Partie",
+        "deployedLink": "https://powerful-fortress-17827.herokuapp.com/"
+    },
     {
         "id": 1,
         "projectName": "CRITTER SITTER",
@@ -18,14 +26,6 @@ const projects = [
         "summary": "A web application that allows a user who wishes to read a book while having a glass of wine paired with it.",
         "githubLink": "https://github.com/lisaworsham/Lit-Wine.git",
         "deployedLink": "https://lisaworsham.github.io/Lit-Wine/"
-    },
-    {
-        "id": 3,
-        "projectName": "NOTE TAKER",
-        "exampleImg": noteTakerImg,
-        "summary": "The purpose of this website is to provide a notetaker app where the user can take notes, store them and then delete them once the task is done. It is to test how I can GET, POST and DELETE something server side with express. And it is deployed via Heroku.",
-        "githubLink": "https://github.com/lisaworsham/noteTaker",
-        "deployedLink": "https://nameless-sands-33784.herokuapp.com/"
     }
 ];
 
